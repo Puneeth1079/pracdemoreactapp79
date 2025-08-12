2357c1652242d16f0de45f8bb177459e3aa3685c
Puneeth <puneeth.achyutha@gmail.com>
2025-08-13 01:31:23 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <h1>Welcome to My React Project - S105</h1>
       <h1>CI/CD and Cloud DevOps Course</h1>
       <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
+      <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
     </div>
   );
 }
